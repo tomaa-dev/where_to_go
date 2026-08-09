bf6df0a05b315cb9ce11578c62927709eb642208
tomaa-dev <lajpanovatoma@gmail.com>
2026-08-09 11:48:56 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -122,3 +122,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
