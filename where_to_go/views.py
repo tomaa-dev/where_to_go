ddabbac470d6d0cf6282184e85b98734c545eacd
tomaa-dev <lajpanovatoma@gmail.com>
2026-08-12 21:51:04 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from django.templatetags.static import static
 
 
 def where_to_go(request):
-    return render(request, 'index.html')
+    places_geojson = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": static('/places/moscow_legends.json')
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": static('/places/roofs24.json')
+          }
+        }
+      ]
+    }
+    context = {
+        'places_geojson': places_geojson
+    }
+    return render(request, 'index.html', context=context)
