e293a0bbeb8d4d8750e762fe7afd2885b57bb2bb
tomaa-dev <lajpanovatoma@gmail.com>
2026-08-14 12:01:18 +0300

    Get preview

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from .models import Place, PlaceImage
+from django.utils.html import format_html
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
+    fields = [('place_image', 'get_preview'), 'position']
+    readonly_fields = ("get_preview",)
+
+    def get_preview(self, obj):
+        return format_html(
+                '<img src="{}" style="max-height: 200px; max-width: 100%; object-fit: contain;" />',
+                obj.place_image.url
+        )
 
 
 class PlaceAdmin(admin.ModelAdmin):
