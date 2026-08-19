fcf5d8f9650725ba64b40cd1f567b1a718e7c96c
tomaa-dev <lajpanovatoma@gmail.com>
2026-08-19 07:44:59 +0300

    Set DEBUG correct

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
